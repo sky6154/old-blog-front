d30bf7900443af1f052c3a5ec0f6f2a8754b9e83
sky6154 <sky6154@gmail.com>
2018-08-22 15:17:13 +0900

    ssh transfer, exec shell test

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -8,7 +8,13 @@ node {
       case "build&deploy":
         runBuild()
         //sh "docker-compose build"
-        sshPublisher(publishers: [sshPublisherDesc(configName: 'Docker Swarm blue1', transfers: [sshTransfer(sourceFiles: 'docker-compose.yml, Dockerfile, build/*')])])
+        sshPublisher(publishers: [
+          sshPublisherDesc(
+            configName: 'Docker Swarm blue1',
+            transfers: [sshTransfer(sourceFiles: 'docker-compose.yml, Dockerfile, build/*')],
+            execCommand: "docker stack deploy -c docker-compose.yml"
+          )
+        ])
       break
       case "build":
         runBuild()
