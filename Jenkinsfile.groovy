4655057062a1a8b1c24844fa8744141de411bd37
sky6154 <sky6154@gmail.com>
2018-08-22 17:57:24 +0900

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -13,8 +13,7 @@ node {
             configName: 'Docker Swarm blue1',
             transfers: [
               sshTransfer(sourceFiles: 'docker-compose.yml, Dockerfile, build/*',
-                          execCommand: "pwd",
-                          execCommand: "docker stack deploy -c ./docker-compose.yml blog-front")
+                          execCommand: "pwd && docker stack deploy -c ./docker-compose.yml blog-front")
             ],
           )
         ])
