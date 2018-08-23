391680902ceeaf15b69def257ad3cd93f6287377
sky6154 <sky6154@gmail.com>
2018-08-23 16:24:16 +0900

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+docker service rm blog-front
+docker rmi blog-front:latest
+
+docker load < blog-front.tar
+service create --name blog-front --replicas 3 --publish 80:80 blog-front:latest
