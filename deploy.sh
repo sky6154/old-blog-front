4b218355c9f67969abe3e68c8a87677c3c85c1d1
sky6154 <sky6154@gmail.com>
2018-08-23 16:46:07 +0900

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-
-docker service rm blog-front
-docker rmi blog-front:latest
+{
+  docker service rm blog-front
+}
+{
+  docker rmi blog-front:latest
+}
 
 docker load < blog-front.tar
 service create --name blog-front --replicas 3 --publish 80:80 blog-front:latest
